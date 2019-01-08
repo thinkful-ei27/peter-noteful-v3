8158f3b7d05fbc7f929eed919c988559e2d96532
Peter Ramos <peterdramos02@gmail.com>
2019-01-08 16:22:42 -0500

    add updatebyid and create method to scratch

--- scratch/queries.js
+++ scratch/queries.js
@@ -14,6 +14,39 @@ const Note = require('../models/note');
 
 //     return Note.find(filter).sort({ updatedAt: 'desc' });
 //   })
+//   .then((results) => console.log(results[0]))
+//   .then(() => mongoose.disconnect())
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+// mongoose.connect(MONGODB_URI,   {useNewUrlParser: true})
+//   .then(() => {
+//     const searchId = '111111111111111111111101';
+//     let filter = {};
+
+//     filter._id = searchId;
+
+//     return Note.findById(filter);
+//   })
+//   .then((results) => console.log(results))
+//   .then(() => mongoose.disconnect())
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//     mongoose.disconnect();
+//   });
+
+// mongoose.connect(MONGODB_URI,   {useNewUrlParser: true})
+//   .then(() => {
+//     const note = {
+//       title: 'This is fine',
+//       content: 'no really its fine'
+//     };
+
+//     return Note.create(note);
+//   })
 //   .then((results) => console.log(results))
 //   .then(() => mongoose.disconnect())
 //   .catch(err => {
@@ -21,19 +54,44 @@ const Note = require('../models/note');
 //     console.error(err);
 //   });
 
-mongoose.connect(MONGODB_URI,   {useNewUrlParser: true})
+// mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
+//   .then(() => {
+
+//     const searchId = {_id: '5c3510f623279e128f2a350b'};
+//     const updatedNote = {
+//       title: 'sdkfjhsdfkjhsdkjhsdkjfh',
+//       content: 'It\'s 4:20 muthaFer'
+//     };
+
+//     return Note.findByIdAndUpdate(searchId, updatedNote, {
+//       new: true,
+//       upsert: true
+//     });
+//   })
+//   .then((results) => console.log(results))
+//   .then(() => mongoose.disconnect())
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
   .then(() => {
-    // const searchId = '111111111111111111111101';
-    let filter = {};
 
-    filter._id = searchId;
+    const searchId = {_id: '5c3510f623279e128f2a350b'};
+    const updatedNote = {
+      title: 'sdkfjhsdfkjhsdkjhsdkjfh',
+      content: 'It\'s 4:20 muthaFer'
+    };
 
-    return Note.findById(filter);
+    return Note.findByIdAndUpdate(searchId, updatedNote, {
+      new: true,
+      upsert: true
+    });
   })
   .then((results) => console.log(results))
   .then(() => mongoose.disconnect())
   .catch(err => {
     console.error(`ERROR: ${err.message}`);
     console.error(err);
-    mongoose.disconnect();
   });
