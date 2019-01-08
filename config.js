b87892f2241cb75211c076c652e3bb2bfd6a0a4c
Peter Ramos <peterdramos02@gmail.com>
2019-01-08 14:48:16 -0500

    run npm i and npm i mongoose change config.js file to use Mongo Uri

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  PORT: process.env.PORT || 8080
+  PORT: process.env.PORT || 8080,
+  MONGODB_URI: process.env.MONGODB_URI || 'mongo.db://localhost:27027/noteful'
 };
