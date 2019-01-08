8e75274673ccca28535b3be327c8ce77e0e8be17
Peter Ramos <peterdramos02@gmail.com>
2019-01-08 17:33:24 -0500

    add get all route

--- routes/notes.js
+++ routes/notes.js
@@ -1,19 +1,28 @@
 'use strict';
 
 const express = require('express');
-
+const Note = require('../models/note');
 const router = express.Router();
 
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
 
-  console.log('Get All Notes');
-  res.json([
-    { id: 1, title: 'Temp 1' },
-    { id: 2, title: 'Temp 2' },
-    { id: 3, title: 'Temp 3' }
-  ]);
+  
+  const searchTerm = req.query.title || req.query.content;
+  console.log(req.query);
 
+  let regex;
+
+  if (searchTerm) {
+    regex = new RegExp(searchTerm, 'i');
+  }
+
+  Note.find({$or: [{}, {title: regex}, {content: regex}]}).sort({ updatedAt: 'desc' })
+    .then((results) => res.json(results))
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      next(err);
+    });
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
