46034bfd485c2e7aa9aceccedda847799492e575
Peter Ramos <peterdramos02@gmail.com>
2019-01-10 16:01:19 -0500

    add folder info to utils/seed-database

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/folder');
 
-const { notes } = require('../db/data');
+const { folders, notes } = require('../db/data');
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(notes))
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(notes),
+      Folder.insertMany(folders),
+      Folder.createIndexes(),
+    ]);
+  })
   .then(results => {
     console.info(`Inserted ${results.length} Notes`);
   })
