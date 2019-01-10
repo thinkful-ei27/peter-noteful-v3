f7ce48110731fbf0c18900e96561148b6ed09a56
Peter Ramos <peterdramos02@gmail.com>
2019-01-10 15:38:16 -0500

    add folderId field to noteSchema

--- models/note.js
+++ models/note.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  content: String
+  content: String,
+  folderId: [{type: mongoose.Schema.Types.ObjectId, ref: 'Folder'}]
 });
 
 // Add `createdAt` and `updatedAt` fields
