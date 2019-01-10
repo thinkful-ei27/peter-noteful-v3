80a79529ce086f6916ce0efef3ce3cb862fdaa18
Peter Ramos <peterdramos02@gmail.com>
2019-01-10 15:35:09 -0500

    add folderSchema to models dir

---
+++ models/folder.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const folderSchema = new mongoose.Schema({
+  title: {
+    type: String, 
+    required: true,
+    unique: true
+  }
+});
+
+folderSchema.set('timestamps', true);
+
+folderSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+module.exports(mongoose.model('Folder', folderSchema));
