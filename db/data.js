aa1ce985b3afb06d100b1db05d14e82e19ba95a2
Peter Ramos <peterdramos02@gmail.com>
2019-01-11 16:25:33 -0500

    add tags data to db/data.js

--- db/data.js
+++ db/data.js
@@ -68,4 +68,23 @@ const folders = [
   }
 ];
 
+const tags = [
+  {
+    "_id": "222222222222222222222200",
+    "name": "breed"
+  },
+  {
+    "_id": "222222222222222222222201",
+    "name": "hybrid"
+  },
+  {
+    "_id": "222222222222222222222202",
+    "name": "domestic"
+  },
+  {
+    "_id": "222222222222222222222203",
+    "name": "feral"
+  }
+];
+
 module.exports = { notes, folders };
