6f1facefd71b94aea2602cb9bb90b6e8a7e219fb
Peter Ramos <peterdramos02@gmail.com>
2019-01-11 16:41:53 -0500

    add tags data to notes data in db/data.js

--- db/data.js
+++ db/data.js
@@ -3,25 +3,29 @@ const notes = [
     '_id': '111111111111111111111101',
     'title': '5 life lessons learned from cats',
     'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    'folderId': '111111111111111111111100'
+    'folderId': '111111111111111111111100',
+    'tags': ['222222222222222222222200', '222222222222222222222201', '222222222222222222222202']
   },
   {
     '_id': '111111111111111111111102',
     'title': 'What the government doesn\'t want you to know about dogs',
     'content': 'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
-    'folderId': '111111111111111111111100'
+    'folderId': '111111111111111111111100',
+    'tags': ['222222222222222222222201', '222222222222222222222202']
   },
   {
     '_id': '111111111111111111111103',
     'title': 'The most boring article about cats you\'ll ever read',
     'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    'folderId': '111111111111111111111101'
+    'folderId': '111111111111111111111101',
+    'tags': ['222222222222222222222200', '222222222222222222222202']
   },
   {
     '_id': '111111111111111111111104',
     'title': 'The most incredible article about dogs you\'ll ever read',
     'content': 'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
-    'folderId': '111111111111111111111102'
+    'folderId': '111111111111111111111102',
+    'tags': ['222222222222222222222202', '222222222222222222222203']
   },
   {
     '_id': '111111111111111111111105',
@@ -33,19 +37,22 @@ const notes = [
     '_id': '111111111111111111111106',
     'title': '10 ways dogs can help you live to 100',
     'content': 'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
-    'folderId': '111111111111111111111103'
+    'folderId': '111111111111111111111103',
+    'tags': ['222222222222222222222201', '222222222222222222222202']
   },
   {
     '_id': '111111111111111111111107',
     'title': '9 reasons you can blame the recession on cats',
     'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    'folderId': '111111111111111111111101'
+    'folderId': '111111111111111111111101',
+    'tags': ['222222222222222222222200', '222222222222222222222203']
   },
   {
     '_id': '111111111111111111111108',
     'title': '10 ways marketers are making you addicted to dogs',
     'content': 'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
-    'folderId': '111111111111111111111102'
+    'folderId': '111111111111111111111102',
+    'tags': ['222222222222222222222200', '222222222222222222222201']
   }
 ];
 
