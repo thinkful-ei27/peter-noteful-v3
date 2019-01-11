83b93d45ad7668544f34e6bf9b69c911dac420c2
Peter Ramos <peterdramos02@gmail.com>
2019-01-11 15:46:37 -0500

    update notes tests to include tests relating to folders

--- test/notes.test.js
+++ test/notes.test.js
@@ -89,6 +89,24 @@ describe('Notes API resouce', function () {
           expect(resNote.content).to.equal(note.content);
         });
     });
+
+    it('should return correct search results for a folderId query', function () {
+      let data;
+      return Folder.findOne()
+        .then((_data) => {
+          data = _data;
+          return Promise.all([
+            Note.find({ folderId: data.id }),
+            chai.request(app).get(`/api/notes?folderId=${data.id}`)
+          ]);
+        })
+        .then(([data, res]) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(data.length);
+        });
+    });
   });
 
   // ================ Test for get single note by id 
